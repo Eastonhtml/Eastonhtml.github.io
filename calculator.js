9bb639487d948fdea55a1d8359d16befc18f64d1
Eastonhtml <31565311+Eastonhtml@users.noreply.github.com>
2017-09-02 17:00:27 +0300

    4

--- calculator.js
+++ calculator.js
@@ -41,4 +41,36 @@ jQuery("document").ready(function(){
          ploshad = 2*((vl8+vl9)+(vl8+vl10)+(vl9+vl10))
          alert(ploshad);
 });
+    function GetResult(oper)
+{ 
+  var n1 = 0;
+  var n2 = 0;
+    	
+  n1 = eval(calcform.num_01.value);   //Число 1
+  n2 = eval(calcform.num_02.value);   //Число 2
+  
+  switch (oper)                       //Проверяем, какая кнопка
+  {                                   //была нажата
+    case 1:   //Кнопка +
+    {
+      calcform.res.value = n1 + n2;
+      break;
+    }
+    case 2:   //Кнопка -
+    {
+      calcform.res.value = n1 - n2;
+      break;
+    }
+    case 3:   //Кнопка *
+    {
+      calcform.res.value = n1 * n2;
+      break;
+    }
+    case 4:   //Кнопка /
+    {
+      calcform.res.value = n1 / n2;
+      break;
+    }
+  }
+}
     });
